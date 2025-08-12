f432a4b7947f85b84a2ecb36e950dc3018883692
Dinarte Filho <dinarte_filho@hotmail.com>
2025-08-12 01:29:25 -0300

    feat: add ContaTerminal program to collect user input and display account details

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,22 +4,22 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Informe o seu nome completo: ");
-        String nome = sc.nextLine(); 
+        System.out.println("Por favor, informe o número da sua conta: ");
+        int conta = sc.nextInt();
+        sc.nextLine();
 
         System.out.println("Por favor, informe o número da sua Agência: ");
         String agencia = sc.nextLine();
 
-        System.out.println("Por favor, informe o número da sua conta: ");
-        int conta = sc.nextInt();
+        System.out.println("Informe o seu nome completo: ");
+        String nome = sc.nextLine(); 
 
         System.out.println("Por favor, informe o seu saldo (use vírgula para centavos): ");
         float saldo = sc.nextFloat();
 
-        System.out.println("O seu nome é " + nome);
-        System.out.println("A sua agência é " + agencia);
-        System.out.println("O número da sua conta é " + conta);
-        System.out.printf("O saldo da sua conta é %.2f\n" , saldo);
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                            + ", conta " + conta + ", e o seu saldo " + saldo +
+                            " já está disponível para saque.");
 
         sc.close();
     }
